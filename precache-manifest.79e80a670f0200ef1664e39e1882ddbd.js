e684c0720361201e108da40249c02054e2153b1c
Gaurav Parashar <gaurav@bbd.co.za>
2020-02-08 02:56:41 +0530

    Updates

---
+++ precache-manifest.79e80a670f0200ef1664e39e1882ddbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdf3883c70d00e4c073f2d112697497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e0f2e6ead08952c3de",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5ff87dff4c8e90fb414c",
+    "url": "/static/js/2.af547405.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.af547405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e0f2e6ead08952c3de",
+    "url": "/static/js/main.c5d790a8.chunk.js"
+  },
+  {
+    "revision": "7ef4209dacfc08641ebb",
+    "url": "/static/js/runtime-main.786742d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

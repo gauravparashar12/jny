77a227830bd196eee37e65c0c7559bfddd2f11d0
Gaurav Parashar <gaurav@bbd.co.za>
2020-02-10 11:39:29 +0530

    first story

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Slow and steady wins the race
       </header>
+      <div>
+      There once was a speedy Hare who bragged about how fast he could run. Tired of hearing him boast, the Tortoise challenged him to a race. All the animals in the forest gathered to watch.
+
+The Hare ran down the road for a while and then paused to rest. He looked back at the tortoise and cried out, "How do you expect to win this race when you are walking along at your slow, slow pace?"
+
+The Hare stretched himself out alongside the road and fell asleep, thinking, "There is plenty of time to relax.
+
+The Tortoise walked and walked, never ever stopping until he came to the finish line.
+
+The animals who were watching cheered so loudly for Tortoise that they woke up the Hare. The Hare stretched, yawned and began to run again, but it was too late. Tortoise had already crossed the finish line.
+      </div>
+
     </div>
   );
 }

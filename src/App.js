bfec426d5ad5cd07d96473b915c976e19210e7f1
Gaurav Parashar <gaurav@bbd.co.za>
2020-02-10 11:51:16 +0530

    first story modify

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Slow and steady wins the race
-      </header>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <b>  Slow and steady wins the race</b>
+    
       <div>
       There once was a speedy Hare who bragged about how fast he could run. Tired of hearing him boast, the Tortoise challenged him to a race. All the animals in the forest gathered to watch.
 
@@ -20,7 +20,7 @@ The Tortoise walked and walked, never ever stopping until he came to the finish 
 
 The animals who were watching cheered so loudly for Tortoise that they woke up the Hare. The Hare stretched, yawned and began to run again, but it was too late. Tortoise had already crossed the finish line.
       </div>
-
+      </header>
     </div>
   );
 }
